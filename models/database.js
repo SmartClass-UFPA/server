0cae905efcdf7f683d00bfb4aaa128974e46a744
Derian Fernando <derian.sobral@gmail.com>
2017-01-21 01:15:01 -0200

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,3 +1,4 @@
+
 var pg = require('pg');
 var config = {
   user: 'smartclass', //env var: PGUSER
@@ -9,22 +10,22 @@ var config = {
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
-var createTableUsers = function() {
+var createTableEmentas = function() {
   const client = new pg.Client(config);
 
   //making table for users
   client.connect();
-  query = client.query('CREATE TABLE IF NOT EXISTS users(id_user INT NOT NULL, name VARCHAR(45) NOT NULL, nickname VARCHAR(45) NOT NULL, cpf VARCHAR(11) NOT NULL, adress TEXT NULL, matricula VARCHAR(45) NULL, email VARCHAR(45) NULL, nascimento DATE NULL, sexo VARCHAR(1) NULL,  PRIMARY KEY (id_user))');
-  query.on('end', () => { client.end(); console.log("Tabela users criada com sucesso\n");});
+  query = client.query('CREATE TABLE IF NOT EXISTS ementas(n_curso INTEGER PRIMARY KEY, semestre INTEGER NOT NULL, materia VARCHAR(40) NOT NULL UNIQUE, assuntos TEXT, livros TEXT, ch INTEGER, cod VARCHAR(10))');
+  query.on('end', () => { client.end(); console.log("Tabela ementas criada com sucesso\n");});
 };
 
-var createTableBikes = function() {
+var createTableAtividades = function() {
   const client = new pg.Client(config);
 
   //making table for bikes
   client.connect();
-  query = client.query('CREATE TABLE IF NOT EXISTS bikes(id_bike INT NOT NULL, status VARCHAR(20) NOT NULL, cont INT NOT NULL, PRIMARY KEY (id_bike), CONSTRAINT station_bike FOREIGN KEY (id_bike) REFERENCES stations (id_station) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT user_bike FOREIGN KEY (id_bike) REFERENCES users (id_user) ON DELETE NO ACTION ON UPDATE NO ACTION)');
-  query.on('end', () => { client.end(); console.log("Tabela bikes criada com sucesso\n");});
+  query = client.query('CREATE TABLE IF NOT EXISTS atividades(data_entrega DATE NOT NULL, materia VARCHAR(40) NOT NULL,FOREIGN KEY (materia) REFERENCES ementas (materia), tipo VARCHAR(1) NOT NULL, info TEXT, semestre INTEGER NOT NULL, turno VARCHAR(1),n_curso INTEGER NOT NULL,FOREIGN KEY (n_curso) REFERENCES ementas (n_curso))');
+  query.on('end', () => { client.end(); console.log("Tabela atividades criada com sucesso\n");});
 };
 
 var createTableStations = function() {
@@ -44,7 +45,7 @@ var createTableLoans = function() {
   query = client.query('CREATE TABLE IF NOT EXISTS loans(id_loan INT NOT NULL, date DATE NOT NULL, time TIME NOT NULL, type VARCHAR(1) NOT NULL, PRIMARY KEY (id_loan), CONSTRAINT loan_user FOREIGN KEY (id_loan) REFERENCES users (id_user) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT loan_station FOREIGN KEY (id_loan) REFERENCES stations (id_station) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT loan_bike FOREIGN KEY (id_loan) REFERENCES bikes (id_bike) ON DELETE NO ACTION ON UPDATE NO ACTION)');
   query.on('end', () => { client.end(); console.log("Tabela emprestimo criada com sucesso\n");});
 };
-createTableUsers();
-createTableStations();
+createTableEmentas();
+createTableAtividades();
 createTableBikes();
 createTableLoans();
