4553975f292ffd2bec7109c2c1b8134398fb4f2b
Cleverson Nahum <cleversonahum@gmail.com>
2017-01-21 22:17:48 -0300

    tabela horarios_aula criada fix #3

--- models/database.js
+++ models/database.js
@@ -4,7 +4,7 @@ var config = {
   user: 'smartclass', //env var: PGUSER
   database: 'smartclass', //env var: PGDATABASE
   password: 'mysecretpassword', //env var: PGPASSWORD
-  host: '172.17.0.2', // Server hosting the postgres database POR ALGUM MOTIVO A PORTA DO LOCALHOST N TAVA FUNCIONANDO
+  host: '172.17.0.3', // Server hosting the postgres database POR ALGUM MOTIVO A PORTA DO LOCALHOST N TAVA FUNCIONANDO
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
@@ -28,24 +28,15 @@ var createTableAtividades = function() {
   query.on('end', () => { client.end(); console.log("Tabela atividades criada com sucesso\n");});
 };
 
-var createTableStations = function() {
+var createTableHorariosAula = function() {
   const client = new pg.Client(config);
 
-  //making table for station
+  //making table for bikes
   client.connect();
-  query = client.query('CREATE TABLE IF NOT EXISTS stations(id_station INT NOT NULL, name VARCHAR(45) NULL, qnt_slots INT NOT NULL, adress TEXT NOT NULL, connected BIT NULL, PRIMARY KEY (id_station))');
-  query.on('end', () => { client.end(); console.log("Tabela stations criada com sucesso\n"); });
+  query = client.query('CREATE TABLE IF NOT EXISTS horarios_aula(semestre INTEGER NOT NULL, turno VARCHAR(1) NOT NULL,materia1 VARCHAR(40), materia2 VARCHAR(40), materia3 VARCHAR(40), n_curso INTEGER NOT NULL, dia_semana INTEGER NOT NULL)');
+  query.on('end', () => { client.end(); console.log("Tabela horarios_aula criada com sucesso\n");});
 };
 
-var createTableLoans = function() {
-  const client = new pg.Client(config);
-
-  //making table for loans
-  client.connect();
-  query = client.query('CREATE TABLE IF NOT EXISTS loans(id_loan INT NOT NULL, date DATE NOT NULL, time TIME NOT NULL, type VARCHAR(1) NOT NULL, PRIMARY KEY (id_loan), CONSTRAINT loan_user FOREIGN KEY (id_loan) REFERENCES users (id_user) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT loan_station FOREIGN KEY (id_loan) REFERENCES stations (id_station) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT loan_bike FOREIGN KEY (id_loan) REFERENCES bikes (id_bike) ON DELETE NO ACTION ON UPDATE NO ACTION)');
-  query.on('end', () => { client.end(); console.log("Tabela emprestimo criada com sucesso\n");});
-};
 createTableEmentas();
 createTableAtividades();
-createTableBikes();
-createTableLoans();
+createTableHorariosAula();
