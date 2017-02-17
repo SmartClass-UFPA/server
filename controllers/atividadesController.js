a4e4ffa198077529174226693f81fe023d0ac041
Helderanr <helderanr@gmail.com>
2017-02-17 00:55:14 +0000

    Update atividadesController.js

--- controllers/atividadesController.js
+++ controllers/atividadesController.js
@@ -10,7 +10,7 @@ var config = {
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
 
-// Adicionar Ementa
+// Adicionar Atividade
 exports.addAtividade = function(req, res, next) {
   const results = [];
   // Grab data from http request
@@ -43,6 +43,9 @@ exports.addAtividade = function(req, res, next) {
 // Ler as atividades cadastradas
 exports.readAtividades = function(req, res, next) {
   const results = [];
+  const n_curso = req.params.n_curso;
+  const semestre = req.params.semestre;
+  const turno = req.params.turno;
   // Get a Postgres client from the connection pool
   pg.connect(config, (err, client, done) => {
     // Handle connection errors
@@ -52,7 +55,36 @@ exports.readAtividades = function(req, res, next) {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM atividades WHERE materia=($1) ORDER BY id ASC;');
+    const query = client.query('SELECT * FROM atividades WHERE n_curso=($1) AND semestre=($2) AND turno=($3) ORDER BY data_entrega ASC;', [n_curso,semestre,turno]);
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+}
+
+// Ler as atividades cadastradas de uma matéria
+exports.readAtividades = function(req, res, next) {
+  const results = [];
+  const n_curso = req.params.n_curso;
+  const semestre = req.params.semestre;
+  const turno = req.params.turno;
+  const materia = req.params.materia;
+  // Get a Postgres client from the connection pool
+  pg.connect(config, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM atividades WHERE n_curso=($1) AND semestre=($2) AND turno=($3) AND materia=($4) ORDER BY data_entrega ASC;', [n_curso,semestre,turno, materia]);
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
@@ -67,7 +99,7 @@ exports.readAtividades = function(req, res, next) {
 
 // Update atividades
 exports.updateAtividades = function(req, res, next) {
-  const results = [];
+  const results = []; 
   // Grab data from the URL parameters and from http requester
   const data = {data_entrega: req.body.data_entrega, materia: req.body.materia, tipo: req.body.tipo, info: req.body.info, semestre: req.body.semestre, turno: req.body.turno, n_curso: req.body.n_curso};
 
@@ -80,8 +112,8 @@ exports.updateAtividades = function(req, res, next) {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Update Data
-    client.query('UPDATE atividades SET n_curso=($1), materia=($2), tipo=($3), info=($4), semestre=($5), turno=($6) WHERE data_entrega=($7)',
-    [data.semestre, data.materia, data.assuntos, data.livros, data.ch, data.cod, data.n_curso]);
+    client.query('UPDATE atividades SET tipo=($1), info=($2) WHERE n_curso=($3), semestre=($4), turno($5), materia=($6), data_entrega=($7);',
+    [data.tipo, data.info, data.n_curso, data.semestre, data.turno, data.materia, data.data_entrega]);
     // SQL Query > Select Data
     const query = client.query("SELECT * FROM atividades ORDER BY data_entrega ASC");
     // Stream results back one row at a time
@@ -94,13 +126,15 @@ exports.updateAtividades = function(req, res, next) {
       return res.json(results);
     });
   });
-}
+}				
 
-//Delete Bike
+//Delete Atividades da turma
 exports.delAtividade = function(req, res, next) {
   const results = [];
   // Grab data from the URL parameters
-  const id = req.params.todo_id;
+  const n_curso = req.params.n_curso;
+  const semestre = req.params.semestre;
+  const turno = req.params.turno;
   // Get a Postgres client from the connection pool
   pg.connect(config, (err, client, done) => {
     // Handle connection errors
@@ -110,7 +144,68 @@ exports.delAtividade = function(req, res, next) {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Delete Data
-    client.query('DELETE FROM atividades WHERE n_curso=($1)', [id]);
+    client.query('DELETE FROM atividades WHERE n_curso=($1) AND semestre=($2) AND turno=($3);', [n_curso,semestre,turno]);
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM atividades ORDER BY n_curso ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+}
+
+//Delete Atividades da materia
+exports.delAtividade = function(req, res, next) {
+  const results = [];
+  // Grab data from the URL parameters
+  const n_curso = req.params.n_curso;
+  const semestre = req.params.semestre;
+  const turno = req.params.turno;
+  const materia = req.params.materia;
+  // Get a Postgres client from the connection pool
+  pg.connect(config, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Delete Data
+    client.query('DELETE FROM atividades WHERE n_curso=($1) AND semestre=($2) AND turno=($3) AND materia=($4);', [n_curso,semestre,turno,materia]);
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM atividades ORDER BY n_curso ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+}
+
+//Delete uma Atividade da materia
+exports.delAtividade = function(req, res, next) {
+  const results = [];
+  // Grab data from the URL parameters
+  const data = {data_entrega: req.body.data_entrega, materia: req.body.materia, semestre: req.body.semestre, turno: req.body.turno, n_curso: req.body.n_curso};
+  // Get a Postgres client from the connection pool
+  pg.connect(config, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Delete Data
+    client.query('DELETE FROM atividades WHERE n_curso=($1) AND semestre=($2) AND turno=($3) AND materia=($4) AND data_entrega=($5);', [data.n_curso,data.semestre,data.turno,data.materia, data.data_entrega]);
     // SQL Query > Select Data
     var query = client.query('SELECT * FROM atividades ORDER BY n_curso ASC');
     // Stream results back one row at a time
